7c055a79f4d37151ee9d1c421a99a3e2996f6703
waterproofpatch <waterproofpatch@gmail.com>
2019-03-02 13:36:01 -0800

    install_plugins

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dotfiles
+Personal repo for dotfiles and installer scripts to help setting up new dev environments.
+
+## Usage
+
+```bash
+./install_dotfiles.sh
+./install_plugins.sh
+```
---
+++ install_plugins.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ ! -d "~/.vim/autoload" ]; then
+        echo "Installing pathogen..."
+        mkdir -p ~/.vim/autoload
+        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+if [ ! -d "~/.vim/bundle" ]; then
+        echo "Creating bundle dir..."
+        mkdir -p ~/.vim/bundle
+fi
+
+echo "Installing plugin dependencies..."
+sudo apt-get install exuberant-ctags
+
+echo "Installing plugins from github..."
+git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/bundle/vim-tmux-navigator || true
+git clone https://github.com/w0rp/ale.git ~/.vim/bundle/ale || true
+git clone https://github.com/posva/vim-vue.git ~/.vim/bundle/vim-vue || true
+git clone https://github.com/rhysd/vim-clang-format.git ~/.vim/bundle/vim-clang-format || true
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp || true
+git clone https://github.com/itchyny/lightline.vim.git ~/.vim/bundle/lightline || true
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript || true
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter || true
+git clone https://github.com/tpope/vim-surround ~/.vim/bundle/vim-surround || true
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive || true
+git clone https://github.com/xolox/vim-easytags.git ~/.vim/bundle/vim-easytags || true
+git clone https://github.com/xolox/vim-misc.git ~/.vim/bundle/vim-misc || true
