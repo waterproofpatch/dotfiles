ac2ed0812c4bf46e0653d9bee99e9b96c7f463fd
waterproofpatch <waterproofpatch@gmail.com>
2019-04-13 23:17:36 -0400

    autopep8

--- install_plugins.sh
+++ install_plugins.sh
@@ -9,12 +9,16 @@ if [ ! -d "~/.vim/bundle" ]; then
         echo "Creating bundle dir..."
         mkdir -p ~/.vim/bundle
 fi
+if [ ! -d "~/.vim/plugin" ]; then
+        echo "Creating plugin dir..."
+        mkdir -p ~/.vim/plugin
+fi
 
-echo "Installing plugin dependencies..."
-sudo apt-get install exuberant-ctags -y
-
-echo "Installing curl..."
+echo "Installing third party tools used by plugins..."
 sudo apt-get install curl -y
+sudo apt-get install exuberant-ctags -y
+sudo apt-get python3-pip -y
+pip install --user --update autopep8 
 
 echo "Installing pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
@@ -34,4 +38,5 @@ git clone https://github.com/tpope/vim-surround ~/.vim/bundle/vim-surround || tr
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive || true
 git clone https://github.com/xolox/vim-easytags.git ~/.vim/bundle/vim-easytags || true
 git clone https://github.com/xolox/vim-misc.git ~/.vim/bundle/vim-misc || true
-
+git clone https://github.com/tell-k/vim-autopep8.git ~/.vim/bundle/vim-autopep8 || true
+cp ~/.vim/bundle/vim-autopep8/ftplugin/* ~/.vim/plugin/
