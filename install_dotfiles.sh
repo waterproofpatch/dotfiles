f90862aad376f19f93c45bf27483dbadc2ae5184
waterproofpatch <waterproofpatch@gmail.com>
2019-05-04 14:42:30 -0400

    dotfiles for osx vs ubuntu

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -9,4 +9,14 @@ echo "Copying dotfiles from repo to $HOME..."
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 cp .clang-format ~/.clang-format
+
+# some things will depend on the terminal emulator, so use platform specific bash scripts
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "MAC OSX detected, using OSX bash version."
+        cp .bashrc_osx ~/.bashrc
+elif [[ "OSTYPE" == "linux-gnu" ]]; then
+        echo "Linux-GNU detected, using ubuntu bash version"
+        cp .bashrc_ubuntu ~/.bashrc
+fi
+
 echo "Done."
