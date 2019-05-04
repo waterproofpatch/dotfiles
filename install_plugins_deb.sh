8e34efadce634a1ccab60a66a7b23fa6bee21d95
waterproofpatch <waterproofpatch@gmail.com>
2019-05-04 14:33:03 -0400

    checking host type, checking available programs before installing them

--- install_plugins_deb.sh
+++ install_plugins_deb.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "MAC OSX detected, aborting."
+        exit 1
+fi
 
+# create default directories
 if [ ! -d "~/.vim/autoload" ]; then
         echo "Installing pathogen..."
         mkdir -p ~/.vim/autoload
@@ -15,9 +20,24 @@ if [ ! -d "~/.vim/plugin" ]; then
 fi
 
 echo "Installing third party tools used by plugins..."
-sudo apt-get install curl -y
-sudo apt-get install exuberant-ctags -y
-sudo apt-get python3-pip -y
+if ! [ -x "$(command -v git)" ]; then
+        echo 'Error: git is not installed.' >&2
+        sudo apt-get install git -y
+fi
+if ! [ -x "$(command -v curl)" ]; then
+        echo 'Error: curl is not installed.' >&2
+        sudo apt-get install curl -y
+fi
+if ! [ -x "$(command -v ctags)" ]; then
+        echo 'error: exuberant-ctags is not installed.' >&2
+        sudo apt-get install exuberant-ctags -y
+fi
+if ! [ -x "$(command -v pip)" ]; then
+        echo 'error: pip is not installed.' >&2
+        sudo apt-get install python3-pip -y
+fi
+
+echo "Installing autopep8"
 pip install --user --update autopep8 
 
 echo "Installing pathogen..."
@@ -39,4 +59,6 @@ git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive |
 git clone https://github.com/xolox/vim-easytags.git ~/.vim/bundle/vim-easytags || true
 git clone https://github.com/xolox/vim-misc.git ~/.vim/bundle/vim-misc || true
 git clone https://github.com/tell-k/vim-autopep8.git ~/.vim/bundle/vim-autopep8 || true
+git clone https://tpope.io/vim/eunuch.git ~/.vim/bundle/eunuch || true
+
 cp ~/.vim/bundle/vim-autopep8/ftplugin/* ~/.vim/plugin/
