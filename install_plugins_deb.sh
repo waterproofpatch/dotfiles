23975fc46e5ac3218ebde6ed5301c83f65366777
waterproofpatch <waterproofpatch@gmail.com>
2019-06-15 01:14:57 -0400

    refactor, added some missing plugins

--- install_plugins_deb.sh
+++ install_plugins_deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 # Installs vim plugins from git as well as any tools (pip, curl, git etc.) needed for a 
 # basic development environment.
+
 create_dirs() {
         # create default dirs for vim plugins
         if [ ! -d "~/.vim/autoload" ]; then
@@ -19,8 +19,29 @@ create_dirs() {
         fi
 }
 
+make_backups () {
+        timestamp=$(date +"%d-%m-%y_%H-%M-%S")
+        backupDirName=~/.dotfiles.backup_${timestamp}
+        echo "Backing up dotfiles to ${backupDirName}"
+        mkdir -p ${backupDirName}
+        if [ $? -ne 0 ]; then
+                echo "Failed creating backup dir."
+                exit 1
+        fi
+        cp ~/.vimrc ${backupDirName}/
+        cp ~/.tmux.conf ${backupDirName}/
+        cp ~/.bashrc ${backupDirName}/
+        cp -rf ~/.vim ${backupDirName}/
+        if [ $? -ne 0 ]; then
+                echo "Failed backing up ~/.vim dir."
+                exit 1
+        fi
+}
+
 install_from_git () {
         # install vim plugins from various git repos
+        echo "Removing ~/.vim..."
+        rm -rf ~/.vim
         create_dirs
         echo "Installing vim plugins from git..."
         git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/bundle/vim-tmux-navigator || true
@@ -37,12 +58,16 @@ install_from_git () {
         git clone https://github.com/xolox/vim-misc.git ~/.vim/bundle/vim-misc || true
         git clone https://github.com/tell-k/vim-autopep8.git ~/.vim/bundle/vim-autopep8 || true
         git clone https://tpope.io/vim/eunuch.git ~/.vim/bundle/eunuch || true
+        git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar || true
+        git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go || true
+        git clone https://github.com/prettier/vim-prettier ~/.vim/bundle/vim-prettier || true
+        git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter || true
+        git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/vim-colorschemes || true
 
         cp ~/.vim/bundle/vim-autopep8/ftplugin/* ~/.vim/plugin/
 } 
 
-
-install_deps() {
+install_deps_deb() {
         echo "Installing third party tools used by plugins..."
         if ! [ -x "$(command -v git)" ]; then
                 echo 'Error: git is not installed.' >&2
@@ -66,11 +91,61 @@ install_deps() {
         fi
 }
 
+install_deps_darwin() {
+        echo "Installing third party tools used by plugins..."
+        if ! [ -x "$(command -v brew)" ]; then
+                echo "Must install brew to get dependencies using this script."
+                exit 1
+        fi
+        if ! [ -x "$(command -v git)" ]; then
+                echo 'Error: git is not installed.' >&2
+                brew install git -y
+        fi
+        if ! [ -x "$(command -v curl)" ]; then
+                echo 'Error: curl is not installed.' >&2
+                brew install curl -y
+        fi
+        if ! [ -x "$(command -v ctags)" ]; then
+                echo 'error: exuberant-ctags is not installed.' >&2
+                brew install exuberant-ctags -y
+        fi
+        if ! [ -x "$(command -v pip)" ]; then
+                echo 'error: pip is not installed.' >&2
+                brew python3-pip -y
+        fi
+        if ! [ -x "$(command -v clang-format)" ]; then
+                echo 'error: clang-format is not installed.' >&2
+                brew install clang-format -y
+        fi
+}
+
+printUsage () {
+        echo "$0 <os>"
+        echo "os: darwin | debian"
+}
+
+if [ "$#" -ne 1 ]; then
+        printUsage
+        exit 1
+fi
+
+OS=$1
+if [ "$OS" != "darwin" -a "$OS" != "debian" ]; then
+        echo "OS must be one of [darwin, debian]"
+        exit 1
+fi
+
+make_backups
 install_from_git
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ "$OS" == "debian" ]]; then
         echo "Installing dependencies for linux-gnu"
-        install_deps
+        install_deps_deb
+elif [[ "$OS" == "darwin" ]]; then
+        echo "Installing dependencies for darwin18"
+        install_deps_darwin
+else
+        echo "Unknown OS."
         exit 1
 fi
 
@@ -78,7 +153,4 @@ echo "Installing pip tools..."
 pip install autopep8 
 pip install pytest 
 pip install virtualenv 
-
-
-
 
