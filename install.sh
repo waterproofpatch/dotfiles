b97fa0e869cc02350bf02739987a1e875ed7323b
waterproofpatch <waterproofpatch@gmail.com>
2019-06-29 12:54:00 -0400

    vim markdown preview

--- install.sh
+++ install.sh
@@ -68,6 +68,7 @@ install_from_git () {
         git clone https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim || true
         git clone https://github.com/easymotion/vim-easymotion.git ~/.vim/bundle/vim-easymotion || true
         git clone https://github.com/bling/vim-bufferline ~/.vim/bundle/vim-bufferline || true
+        git clone https://github.com/JamshedVesuna/vim-markdown-preview.git ~/.vim/bundle/vim-markdown-preview || true
 
         # this one is strange
         cp ~/.vim/bundle/vim-autopep8/ftplugin/* ~/.vim/plugin/
