0e70b7d1d4d290dbdd70bbd2e7dcfbf39c4a4056
waterproofpatch <waterproofpatch@gmail.com>
2025-03-21 15:18:43 -0400

    run pluginstall

--- install-mac.sh
+++ install-mac.sh
@@ -17,3 +17,6 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install vim plugins
+vim -E +PlugInstall +qall
