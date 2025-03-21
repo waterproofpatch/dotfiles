02cbe6223bc28ef736aa188609b6f7cf95819968
waterproofpatch <waterproofpatch@gmail.com>
2025-03-21 14:54:10 -0400

    add zsh plugins

--- install-mac.sh
+++ install-mac.sh
@@ -3,6 +3,13 @@ cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 cp .fzf.zsh ~/.fzf.zsh
 cp .clang-format ~/.clang-format
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 brew install fzf
 brew install wget
 brew install tmux
